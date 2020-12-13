2a655bf4cd554ca72d9671e3f515b80ba754e04c
Robert Englund <robertenglund980@gmail.com>
2020-12-13 16:01:14 -0600

    news and featured image

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -12,33 +12,4 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
-
-    # Find one record of data from the mongo database
-    # @TODO: YOUR CODE HERE!
-    destination_data = mongo.db.CRWeather.find_one()
-
-    # Return template and data
     return render_template("index.html", vacation=destination_data)
-
-
-# Route that will trigger the scrape function
-@app.route("/scrape")
-def scrape():
-
-    # Run the scrape function and save the results to a variable
-    # @TODO: YOUR CODE HERE!
-    CRWeather = mongo.db.CRWeather
-    CRWeather_data = scrape_costa.scrape_info()
-    # assuming that ".scrape() is a function and not something that was defined in scrape_costa"
-    # bad assumption ".scrape()" needs to be defined as a "def" 
-
-    # Update the Mongo database using update and upsert=True
-    # @TODO: YOUR CODE HERE!
-    CRWeather.update({}, CRWeather_data, upsert=True)
-
-    # Redirect back to home page
-    return redirect("/", code=302)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
